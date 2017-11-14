82d7393b863563b8ca1720ebfa70f7bbe154dade
Jose Pleonasm <jose.pleonasm@gmail.com>
2017-11-14 21:11:11 +0100

    redesign

--- src/js/utils/error.js
+++ src/js/utils/error.js
@@ -1,32 +1,54 @@
 
+/**
+ * A base error class.
+ *
+ * @extends {Error}
+ */
 class LudantonError extends Error {
-	constructor(category, code, ...context) {
+	/**
+	 * @param  {string}    code
+	 * @param  {string}    category
+	 * @param  {...*}      data
+	 */
+	constructor(code, category = LudantonError.Category.GENERAL, ...data) {
+		code = code || '';
 		category = category || 'UNKNOWN';
-		code = code || 'UNKNOWN';
 
-		super(`${category}__${code}`);
+		super(`${category}:${code}`);
 
 		this.category = category;
 		this.code = code;
-		this.context = context;
+		this.data = data;
 	}
 
+	/**
+	 * Returns a string representing the error.
+	 *
+	 * @override
+	 * @return {string}
+	 */
 	toString() {
-		return `LudantonError: ${JSON.stringify(this, null, '  ')}`;
+		return `LudantonError: ${JSON.stringify(this)}`;
 	}
 }
 
-LudantonError.CATEGORY = {
-	/** Miscellaneous errors. */
-	'GENERAL': 'GENERAL',
-};
-
-LudantonError.CODE = {
+/**
+ * @enum {string}
+ */
+LudantonError.Code = {
 	/**
-	 * {string} error.context[0] URI
-	 * {string} error.context[1] HTTP status
+	 * {string} error.data[0] URI
+	 * {string} error.data[1] HTTP status
 	 */
 	'BAD_HTTP_STATUS': 'BAD_HTTP_STATUS',
+};
+
+/**
+ * @enum {string}
+ */
+LudantonError.Category = {
+	/** Miscellaneous errors. */
+	'GENERAL': 'GENERAL',
 };
 
 export default LudantonError;
