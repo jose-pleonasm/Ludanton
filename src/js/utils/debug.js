3f076a089b8bd2312c2c7725432403f0f607399f
Jose Pleonasm <jose.pleonasm@gmail.com>
2019-04-20 13:38:08 +0200

    cleaning

--- src/js/utils/debug.js
+++
@@ -1,8 +0,0 @@
-
-export const debugPlaybackState = (element) => {
-	console.debug(element.src);
-	console.debug(element.getElementsByTagName('source'));
-	console.debug(element.currentSrc);
-	console.debug('paused:', element.paused);
-	console.debug('played:', element.played);
-};
